d94229b1e5a9f70c4726e50064ad83bc6ae729ac
Jeremy-Burns0212 <208514124+Jeremy-Burns0212@users.noreply.github.com>
2026-01-14 15:35:10 -0800

    Rename Labels and TextBoxes to fit functionality

--- WinFormsSourceControl/Form1.Designer.cs
+++ WinFormsSourceControl/Form1.Designer.cs
@@ -30,9 +30,9 @@
         {
 			this.label1 = new System.Windows.Forms.Label();
 			this.label2 = new System.Windows.Forms.Label();
-			this.textBox1 = new System.Windows.Forms.TextBox();
-			this.textBox2 = new System.Windows.Forms.TextBox();
-			this.button1 = new System.Windows.Forms.Button();
+			this.txtLength = new System.Windows.Forms.TextBox();
+			this.txtWidth = new System.Windows.Forms.TextBox();
+			this.btnCalcAreaAndPerimeter = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
 			// label1
@@ -40,50 +40,50 @@
 			this.label1.AutoSize = true;
 			this.label1.Location = new System.Drawing.Point(45, 65);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(51, 20);
+			this.label1.Size = new System.Drawing.Size(71, 24);
 			this.label1.TabIndex = 0;
-			this.label1.Text = "label1";
+			this.label1.Text = "Length";
 			// 
 			// label2
 			// 
 			this.label2.AutoSize = true;
 			this.label2.Location = new System.Drawing.Point(45, 108);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(51, 20);
+			this.label2.Size = new System.Drawing.Size(60, 24);
 			this.label2.TabIndex = 1;
-			this.label2.Text = "label2";
+			this.label2.Text = "Width";
 			// 
-			// textBox1
+			// txtLength
 			// 
-			this.textBox1.Location = new System.Drawing.Point(164, 65);
-			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(100, 26);
-			this.textBox1.TabIndex = 2;
+			this.txtLength.Location = new System.Drawing.Point(164, 65);
+			this.txtLength.Name = "txtLength";
+			this.txtLength.Size = new System.Drawing.Size(100, 26);
+			this.txtLength.TabIndex = 2;
 			// 
-			// textBox2
+			// txtWidth
 			// 
-			this.textBox2.Location = new System.Drawing.Point(164, 108);
-			this.textBox2.Name = "textBox2";
-			this.textBox2.Size = new System.Drawing.Size(100, 26);
-			this.textBox2.TabIndex = 3;
+			this.txtWidth.Location = new System.Drawing.Point(164, 105);
+			this.txtWidth.Name = "txtWidth";
+			this.txtWidth.Size = new System.Drawing.Size(100, 26);
+			this.txtWidth.TabIndex = 3;
 			// 
-			// button1
+			// btnCalcAreaAndPerimeter
 			// 
-			this.button1.Location = new System.Drawing.Point(49, 163);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(168, 83);
-			this.button1.TabIndex = 4;
-			this.button1.Text = "button1";
-			this.button1.UseVisualStyleBackColor = true;
+			this.btnCalcAreaAndPerimeter.Location = new System.Drawing.Point(49, 163);
+			this.btnCalcAreaAndPerimeter.Name = "btnCalcAreaAndPerimeter";
+			this.btnCalcAreaAndPerimeter.Size = new System.Drawing.Size(168, 83);
+			this.btnCalcAreaAndPerimeter.TabIndex = 4;
+			this.btnCalcAreaAndPerimeter.Text = "Calculate Area and Perimeter";
+			this.btnCalcAreaAndPerimeter.UseVisualStyleBackColor = true;
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Controls.Add(this.button1);
-			this.Controls.Add(this.textBox2);
-			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.btnCalcAreaAndPerimeter);
+			this.Controls.Add(this.txtWidth);
+			this.Controls.Add(this.txtLength);
 			this.Controls.Add(this.label2);
 			this.Controls.Add(this.label1);
 			this.Name = "Form1";
@@ -97,9 +97,9 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtLength;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.Button btnCalcAreaAndPerimeter;
     }
 }
 
