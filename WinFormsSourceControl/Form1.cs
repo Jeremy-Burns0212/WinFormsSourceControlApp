c119955cef292a8037491b324c0c622446f1f118
Jeremy-Burns0212 <208514124+Jeremy-Burns0212@users.noreply.github.com>
2026-01-14 19:00:54 -0800

    converted to block type namespace since GitHub Actions says it only works with C# 7.3 and we're on C# 10.0.0

--- WinFormsSourceControl/Form1.cs
+++ WinFormsSourceControl/Form1.cs
@@ -8,33 +8,35 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WinFormsSourceControl;
-
-
-public partial class Form1 : Form
+namespace WinFormsSourceControl
 {
-    public Form1()
+
+
+    public partial class Form1 : Form
     {
-        InitializeComponent();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcAreaAndPerimeter_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(txtLength.Text, out int length))
+            {
+                MessageBox.Show("Please enter a valid integer for length.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!int.TryParse(txtWidth.Text, out int width))
+            {
+                MessageBox.Show("Please enter a valid integer for width.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            int area = length * width;
+            int perimeter = 2 * (length + width);
+
+            txtArea.Text = "Area is: " + area;
+            txtPerimeter.Text = "Perimeter is: " + perimeter;
+        }
     }
-
-    private void btnCalcAreaAndPerimeter_Click(object sender, EventArgs e)
-    {
-        if (!int.TryParse(txtLength.Text, out int length))
-        {
-			MessageBox.Show("Please enter a valid integer for length.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-			return;
-		}
-
-        if (!int.TryParse(txtWidth.Text, out int width))
-        {
-            MessageBox.Show("Please enter a valid integer for width.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            return;
-		}
-        int area = length * width;
-        int perimeter = 2 * (length + width);
-
-        txtArea.Text = "Area is: " + area;
-        txtPerimeter.Text = "Perimeter is: " + perimeter;
-		    }
 }
